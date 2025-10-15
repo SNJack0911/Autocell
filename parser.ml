54c79277c8da264979ab4bfa3900355e8e259d87
NGUYEN Duc Hai <haiducnguyen1312@gmail.com>
2025-10-15 13:54:58 +0200

    1st commit

---
+++ parser.ml
@@ -0,0 +1,463 @@
+type token =
+  | EOF
+  | DIMENSIONS
+  | END
+  | OF
+  | ASSIGN
+  | COMMA
+  | LBRACKET
+  | RBRACKET
+  | DOT_DOT
+  | DOT
+  | ADD
+  | SUB
+  | MUL
+  | DIV
+  | MOD
+  | LPAREN
+  | RPAREN
+  | ID of (
+# 63 "parser.mly"
+        string
+# 23 "parser.ml"
+)
+  | INT of (
+# 64 "parser.mly"
+       int
+# 28 "parser.ml"
+)
+
+open Parsing
+let _ = parse_error;;
+# 17 "parser.mly"
+
+open Common
+open Ast
+open Printf
+open Symbols
+
+(** Raise a syntax error with the given message.
+	@param msg	Message of the error. *)
+let error msg =
+	raise (SyntaxError msg)
+
+
+(** Restructure the when assignment into selections.
+	@param f	Function to build the assignment.
+	@param ws	Sequence of (expression, conditions) terminated
+				by (expression, NO_COND).
+	@return		Built statement. *)
+let rec make_when f ws =
+	match ws with
+	| [(e, NO_COND)]	->	f e
+	| (e, c)::t			-> IF_THEN(c, f e, make_when f t)
+	| _ -> failwith "whens list not ended by (expression, NO_COND)."
+
+# 57 "parser.ml"
+let yytransl_const = [|
+    0 (* EOF *);
+  257 (* DIMENSIONS *);
+  258 (* END *);
+  259 (* OF *);
+  260 (* ASSIGN *);
+  261 (* COMMA *);
+  262 (* LBRACKET *);
+  263 (* RBRACKET *);
+  264 (* DOT_DOT *);
+  265 (* DOT *);
+  266 (* ADD *);
+  267 (* SUB *);
+  268 (* MUL *);
+  269 (* DIV *);
+  270 (* MOD *);
+  271 (* LPAREN *);
+  272 (* RPAREN *);
+    0|]
+
+let yytransl_block = [|
+  273 (* ID *);
+  274 (* INT *);
+    0|]
+
+let yylhs = "\255\255\
+\001\000\002\000\002\000\004\000\004\000\005\000\003\000\003\000\
+\006\000\006\000\007\000\008\000\008\000\008\000\011\000\011\000\
+\010\000\010\000\010\000\009\000\009\000\009\000\009\000\009\000\
+\009\000\000\000"
+
+let yylen = "\002\000\
+\007\000\003\000\001\000\001\000\003\000\005\000\000\000\002\000\
+\003\000\003\000\005\000\001\000\001\000\001\000\003\000\003\000\
+\003\000\003\000\003\000\002\000\002\000\001\000\001\000\001\000\
+\003\000\002\000"
+
+let yydefred = "\000\000\
+\000\000\000\000\000\000\026\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\004\000\000\000\000\000\007\000\000\000\000\000\
+\002\000\000\000\005\000\000\000\001\000\000\000\000\000\008\000\
+\000\000\006\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\024\000\023\000\022\000\000\000\012\000\013\000\014\000\
+\000\000\000\000\020\000\021\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\011\000\025\000\015\000\016\000\017\000\018\000\
+\019\000"
+
+let yydgoto = "\002\000\
+\004\000\009\000\018\000\010\000\011\000\024\000\036\000\037\000\
+\038\000\039\000\040\000"
+
+let yysindex = "\255\255\
+\245\254\000\000\008\255\000\000\013\255\252\254\026\255\028\255\
+\035\255\033\255\000\000\022\255\023\255\000\000\025\255\031\255\
+\000\000\001\000\000\000\027\255\000\000\029\255\039\255\000\000\
+\040\255\000\000\041\255\000\255\000\255\030\255\000\255\000\255\
+\000\255\000\000\000\000\000\000\021\255\000\000\000\000\000\000\
+\021\255\042\255\000\000\000\000\014\255\000\255\000\255\000\255\
+\000\255\000\255\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000"
+
+let yyrindex = "\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\048\255\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\002\000\000\000\000\000\000\000\
+\003\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000"
+
+let yygindex = "\000\000\
+\000\000\000\000\000\000\000\000\036\000\000\000\034\000\235\255\
+\229\255\000\000\000\000"
+
+let yytablesize = 276
+let yytable = "\001\000\
+\021\000\010\000\009\000\043\000\044\000\022\000\003\000\041\000\
+\005\000\031\000\032\000\045\000\007\000\008\000\033\000\006\000\
+\034\000\035\000\053\000\054\000\055\000\056\000\057\000\046\000\
+\047\000\048\000\049\000\050\000\012\000\052\000\046\000\047\000\
+\048\000\049\000\050\000\013\000\014\000\015\000\020\000\016\000\
+\017\000\007\000\028\000\029\000\026\000\030\000\027\000\042\000\
+\051\000\003\000\019\000\025\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\000\000\000\000\000\000\000\000\000\000\022\000\010\000\
+\009\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\
+\000\000\023\000\010\000\009\000"
+
+let yycheck = "\001\000\
+\000\000\000\000\000\000\031\000\032\000\006\001\018\001\029\000\
+\001\001\010\001\011\001\033\000\017\001\018\001\015\001\003\001\
+\017\001\018\001\046\000\047\000\048\000\049\000\050\000\010\001\
+\011\001\012\001\013\001\014\001\003\001\016\001\010\001\011\001\
+\012\001\013\001\014\001\008\001\002\001\005\001\008\001\018\001\
+\018\001\017\001\004\001\004\001\018\001\005\001\018\001\018\001\
+\007\001\002\001\015\000\018\000\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\255\255\255\255\255\255\255\255\255\255\006\001\006\001\
+\006\001\255\255\255\255\255\255\255\255\255\255\255\255\255\255\
+\255\255\017\001\017\001\017\001"
+
+let yynames_const = "\
+  EOF\000\
+  DIMENSIONS\000\
+  END\000\
+  OF\000\
+  ASSIGN\000\
+  COMMA\000\
+  LBRACKET\000\
+  RBRACKET\000\
+  DOT_DOT\000\
+  DOT\000\
+  ADD\000\
+  SUB\000\
+  MUL\000\
+  DIV\000\
+  MOD\000\
+  LPAREN\000\
+  RPAREN\000\
+  "
+
+let yynames_block = "\
+  ID\000\
+  INT\000\
+  "
+
+let yyact = [|
+  (fun _ -> failwith "parser")
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 6 : int) in
+    let _4 = (Parsing.peek_val __caml_parser_env 3 : 'config) in
+    let _6 = (Parsing.peek_val __caml_parser_env 1 : 'opt_statements) in
+    Obj.repr(
+# 75 "parser.mly"
+ (
+		if _1 != 2 then error "only 2 dimension accepted";
+		(_4, _6)
+	)
+# 245 "parser.ml"
+               : Ast.prog))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : int) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : int) in
+    Obj.repr(
+# 83 "parser.mly"
+  (
+			if _1 >= _3 then error "illegal field values";
+			[("", (0, (_1, _3)))]
+		)
+# 256 "parser.ml"
+               : 'config))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'fields) in
+    Obj.repr(
+# 88 "parser.mly"
+  ( set_fields _1 )
+# 263 "parser.ml"
+               : 'config))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'field) in
+    Obj.repr(
+# 93 "parser.mly"
+  ( [_1] )
+# 270 "parser.ml"
+               : 'fields))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'fields) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'field) in
+    Obj.repr(
+# 95 "parser.mly"
+  (_3 :: _1 )
+# 278 "parser.ml"
+               : 'fields))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 4 : string) in
+    let _3 = (Parsing.peek_val __caml_parser_env 2 : int) in
+    let _5 = (Parsing.peek_val __caml_parser_env 0 : int) in
+    Obj.repr(
+# 100 "parser.mly"
+  (
+			if _3 >= _5 then error "illegal field values";
+			(_1, (_3, _5))
+		)
+# 290 "parser.ml"
+               : 'field))
+; (fun __caml_parser_env ->
+    Obj.repr(
+# 108 "parser.mly"
+  ( NOP )
+# 296 "parser.ml"
+               : 'opt_statements))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 1 : 'opt_statements) in
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'statement) in
+    Obj.repr(
+# 110 "parser.mly"
+  ( _2 )
+# 304 "parser.ml"
+               : 'opt_statements))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'cell) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expressions) in
+    Obj.repr(
+# 116 "parser.mly"
+  (
+			if (fst _1) != 0 then error "assigned x must be 0";
+			if (snd _1) != 0 then error "assigned Y must be 0";
+			SET_CELL (0, _3)
+		)
+# 316 "parser.ml"
+               : 'statement))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : string) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expressions) in
+    Obj.repr(
+# 122 "parser.mly"
+  (
+			NOP
+		)
+# 326 "parser.ml"
+               : 'statement))
+; (fun __caml_parser_env ->
+    let _2 = (Parsing.peek_val __caml_parser_env 3 : int) in
+    let _4 = (Parsing.peek_val __caml_parser_env 1 : int) in
+    Obj.repr(
+# 131 "parser.mly"
+  (
+			if (_2 < -1) || (_2 > 1) then error "x out of range";
+			if (_4 < -1) || (_4 > 1) then error "x out of range";
+			(_2, _4)
+		)
+# 338 "parser.ml"
+               : 'cell))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 140 "parser.mly"
+  ( _1 )
+# 345 "parser.ml"
+               : 'expressions))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'multiplication) in
+    Obj.repr(
+# 142 "parser.mly"
+  ( _1 )
+# 352 "parser.ml"
+               : 'expressions))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'addition) in
+    Obj.repr(
+# 144 "parser.mly"
+  ( _1 )
+# 359 "parser.ml"
+               : 'expressions))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expressions) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 148 "parser.mly"
+        ( printf "+\n"; NONE)
+# 367 "parser.ml"
+               : 'addition))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expressions) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 150 "parser.mly"
+        ( printf "-\n"; NONE)
+# 375 "parser.ml"
+               : 'addition))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expressions) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 155 "parser.mly"
+        ( printf "*\n"; NONE)
+# 383 "parser.ml"
+               : 'multiplication))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expressions) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 157 "parser.mly"
+        ( printf "/\n"; NONE)
+# 391 "parser.ml"
+               : 'multiplication))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 2 : 'expressions) in
+    let _3 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 159 "parser.mly"
+        ( printf "%%\n"; NONE)
+# 399 "parser.ml"
+               : 'multiplication))
+; (fun __caml_parser_env ->
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 163 "parser.mly"
+        ( printf " +\n"; NONE )
+# 406 "parser.ml"
+               : 'expression))
+; (fun __caml_parser_env ->
+    let _2 = (Parsing.peek_val __caml_parser_env 0 : 'expression) in
+    Obj.repr(
+# 165 "parser.mly"
+        ( printf " -\n"; NONE )
+# 413 "parser.ml"
+               : 'expression))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : 'cell) in
+    Obj.repr(
+# 167 "parser.mly"
+  ( printf "[%d,%d]\n" (fst _1) (snd _1); CELL (0, fst _1, snd _1) )
+# 420 "parser.ml"
+               : 'expression))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : int) in
+    Obj.repr(
+# 169 "parser.mly"
+  ( printf "%d\n" _1; CST _1 )
+# 427 "parser.ml"
+               : 'expression))
+; (fun __caml_parser_env ->
+    let _1 = (Parsing.peek_val __caml_parser_env 0 : string) in
+    Obj.repr(
+# 171 "parser.mly"
+  ( printf "%s\n" _1; NONE )
+# 434 "parser.ml"
+               : 'expression))
+; (fun __caml_parser_env ->
+    let _2 = (Parsing.peek_val __caml_parser_env 1 : 'expressions) in
+    Obj.repr(
+# 173 "parser.mly"
+  ( printf "()\n" ; NONE )
+# 441 "parser.ml"
+               : 'expression))
+(* Entry program *)
+; (fun __caml_parser_env -> raise (Parsing.YYexit (Parsing.peek_val __caml_parser_env 0)))
+|]
+let yytables =
+  { Parsing.actions=yyact;
+    Parsing.transl_const=yytransl_const;
+    Parsing.transl_block=yytransl_block;
+    Parsing.lhs=yylhs;
+    Parsing.len=yylen;
+    Parsing.defred=yydefred;
+    Parsing.dgoto=yydgoto;
+    Parsing.sindex=yysindex;
+    Parsing.rindex=yyrindex;
+    Parsing.gindex=yygindex;
+    Parsing.tablesize=yytablesize;
+    Parsing.table=yytable;
+    Parsing.check=yycheck;
+    Parsing.error_function=parse_error;
+    Parsing.names_const=yynames_const;
+    Parsing.names_block=yynames_block }
+let program (lexfun : Lexing.lexbuf -> token) (lexbuf : Lexing.lexbuf) =
+   (Parsing.yyparse yytables 1 lexfun lexbuf : Ast.prog)
